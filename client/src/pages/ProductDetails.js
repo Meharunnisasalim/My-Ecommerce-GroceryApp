08bb210e645b28dc16d3ffbde3f41c761965380d
Meharunnisasalim <142387481+Meharunnisasalim@users.noreply.github.com>
2024-07-15 23:36:24 +0530

    Update ProductDetails.js

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -44,7 +44,7 @@ const ProductDetails = () => {
       <div className="row container product-details">
         <div className="col-md-6">
           <img
-            src={`/api/v1/product/product-photo/${product._id}`}
+            src={`${API_BASE_URL}/api/v1/product/product-photo/${product._id}`}
             className="card-img-top"
             alt={product.name}
             height="300"
@@ -77,7 +77,7 @@ const ProductDetails = () => {
           {relatedProducts?.map((p) => (
             <div className="card m-2" key={p._id}>
               <img
-                src={`/api/v1/product/product-photo/${p._id}`}
+                src={`${API_BASE_URL}/api/v1/product/product-photo/${p._id}`}
                 className="card-img-top"
                 alt={p.name}
               />
