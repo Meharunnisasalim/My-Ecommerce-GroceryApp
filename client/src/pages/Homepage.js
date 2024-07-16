133c2968d6c4a7ae45ee781c1b6e7f5185ef43e6
Meharunnisasalim <142387481+Meharunnisasalim@users.noreply.github.com>
2024-07-16 15:33:36 +0530

    "update Homepage.js"

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -25,7 +25,7 @@ const HomePage = () => {
   //get all cat
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("/api/v1/category/get-category");
+      const { data } = await axios.get(`${API_BASE_URL}/api/v1/category/get-category`);
       if (data?.success) {
         setCategories(data?.category);
       }
